cde7f0391b2fdc7843ac553e230e36427a510fc7
Maciej Dluzen <maciej.dluzen@yahoo.com>
2019-11-30 18:25:04 +0000

    Rev A

---
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -0,0 +1,12 @@
+package pl.coderslab;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+public class AppConfig {
+}
